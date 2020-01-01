836d2ef27ad422bb0c5cef373eea956b601ee6c0
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-01-01 11:49:02 +0100

    doesn't work

--- src/reducers/cars_reducer.js
+++ src/reducers/cars_reducer.js
@@ -1,1 +1,10 @@
-cars_reducer.js
+export default function(state = null, action) {
+  switch (action.type) {
+    case 'FETCH_CARS':
+      return action.payload;
+    case 'REMOVE_CAR':
+      return state.filter((car) => car !== action.payload);
+    default:
+      return state;
+  }
+}
